6dd4106e9ce5589a19ebce705f385808de4c1bb6
y1j2x34 <y1j2x34@qq.com>
2020-02-23 22:38:54 +0800

    feat: detect change factory support

--- src/foundation/lazy.ts
+++ src/foundation/lazy.ts
@@ -5,40 +5,50 @@ type FieldDetector<T> = () => boolean;
 type FieldDetectorFactory<T> = (obj: T) => FieldDetector<T> | Array<FieldDetector<T>>;
 export class Lazy<Class> {
     private detectorFactories = {};
-    public detectChange(...detectorFactories: Array<FieldDetectorFactory<Class>>) {
+    public detectChangeFactories(...detectorFactories: Array<FieldDetectorFactory<Class>>): PropertyDecorator {
         return (targetPrototype: object, propertyKey: string | symbol) => {
-            this.detectorFactories[propertyKey] = detectorFactories;
+            let cachedDetectorFactories = this.detectorFactories[propertyKey];
+            if (!cachedDetectorFactories) {
+                cachedDetectorFactories = [];
+                this.detectorFactories[propertyKey] = cachedDetectorFactories;
+            }
+            cachedDetectorFactories.push(...detectorFactories);
         };
     }
-    public detectFieldChange(...watchFields: Array<string | FieldWatcher<Class>>) {
+    public detectFieldChange(...watchFields: Array<string | FieldWatcher<Class>>): PropertyDecorator {
         return (targetPrototype: object, propertyKey: string | symbol) => {
-            const CACHE_VALUE_KEY = uniqId('__cache_value_' + propertyKey.toString());
-            this.detectChange(
-                ...watchFields.map((watchField: string | FieldWatcher<Class>) => {
-                    if (typeof watchField === 'string') {
-                        return (obj: Class) => {
-                            return () => {
-                                try {
-                                    return obj[CACHE_VALUE_KEY] !== obj[watchField];
-                                } finally {
-                                    obj[CACHE_VALUE_KEY] = obj[watchField];
-                                }
-                            };
+            const detectorFactories = watchFields.map((watchField: string | FieldWatcher<Class>) => {
+                if (typeof watchField === 'string') {
+                    const CACHE_VALUE_KEY = uniqId(`__cache_value_${propertyKey.toString()}-${watchField}`);
+                    return (obj: Class) => {
+                        return () => {
+                            try {
+                                return obj[CACHE_VALUE_KEY] !== obj[watchField];
+                            } finally {
+                                obj[CACHE_VALUE_KEY] = obj[watchField];
+                            }
                         };
-                    } else {
-                        return (obj: Class) => {
-                            return () => {
-                                const currentValue = watchField(obj);
-                                try {
-                                    return obj[CACHE_VALUE_KEY] !== currentValue;
-                                } finally {
-                                    obj[CACHE_VALUE_KEY] = currentValue;
-                                }
-                            };
+                    };
+                } else {
+                    const CACHE_VALUE_KEY = uniqId('__cache_value_' + propertyKey.toString());
+                    return (obj: Class) => {
+                        return () => {
+                            const currentValue = watchField(obj);
+                            try {
+                                return obj[CACHE_VALUE_KEY] !== currentValue;
+                            } finally {
+                                obj[CACHE_VALUE_KEY] = currentValue;
+                            }
                         };
-                    }
-                })
-            );
+                    };
+                }
+            });
+            const cachedDetectorFactories = this.detectorFactories[propertyKey];
+            if (!cachedDetectorFactories) {
+                this.detectorFactories[propertyKey] = detectorFactories;
+            } else {
+                this.detectorFactories[propertyKey].push(...detectorFactories);
+            }
         };
     }
     public property<ReturnType>(
@@ -54,14 +64,16 @@ export class Lazy<Class> {
             }
             const descriptor: PropertyDescriptor = {};
             const lazyThis$ = this;
-            const detectorFactories = lazyThis$.detectorFactories[propertyKey];
             descriptor.get = function() {
                 let detectors: Array<FieldDetector<Class>> = this[detectorsKey];
                 if (!detectors) {
-                    this[detectorsKey] = detectors = detectorFactories.reduce(
-                        (all, factory) => all.concat(factory(this)),
-                        []
-                    );
+                    const detectorFactories = lazyThis$.detectorFactories[propertyKey];
+                    if (detectorFactories) {
+                        this[detectorsKey] = detectors = detectorFactories.reduce(
+                            (all, factory) => all.concat(factory(this)),
+                            []
+                        );
+                    }
                 }
                 if (detectors) {
                     const hasChanged = detectors.some(detector => {
